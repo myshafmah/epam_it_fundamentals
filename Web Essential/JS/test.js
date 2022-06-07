3300fa5edb6443ceafa5f4607e435910c700fcc5
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-07 11:57:14 +0300

    JS Variable Scope

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -3,5 +3,10 @@ function get_average(a,b){
     return average;
 };
 
-var result = get_average(7,8);
-console.log(result);
+var result = get_average(7,11);
+
+function log_result(){
+    console.log('avarage is '+result+ " inside function");
+}
+
+log_result();
