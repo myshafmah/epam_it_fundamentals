ddf1969efe396f91a21a5f5cf0422b165bcb1602
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-07 09:03:18 +0300

    JS If Statements

--- FrontEnd/JS/test.js
+++ FrontEnd/JS/test.js
@@ -1,1 +1,8 @@
-alert("Hello, World!");
+var youLikeMeat = false;
+var mynum = 10;
+
+if (youLikeMeat) {
+    document.write("expresion is true")
+}else{
+    document.write("Expresion not equal 10")
+}
