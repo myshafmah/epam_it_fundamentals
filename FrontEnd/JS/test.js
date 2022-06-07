9cf4508b816ff6885abdb6dc4bc7b1d1f2f57680
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-07 11:44:00 +0300

    JS Functions

--- FrontEnd/JS/test.js
+++ FrontEnd/JS/test.js
@@ -1,4 +1,7 @@
-var links=document.getElementsByTagName('a');
-for(var i=0; i<links.length; i++){
-    links[i].className='link='+i;
-}
+function get_average(a,b){
+    var average=(a+b)/2;
+    return average;
+};
+
+var result = get_average(7,8);
+console.log(result);
