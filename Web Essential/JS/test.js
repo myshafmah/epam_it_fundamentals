73e4ac7089029db4eaddfeb8e28749bb7b29fce1
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-08 10:21:09 +0300

    JS Introduction to Objects

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -1,18 +1,13 @@
-var arr = [];
-arr[0]=25;
-arr[1]=35;
-arr[2]=true;
-arr[3]="Hello";
-arr[2]=false;
-var arr2 = [10,20,"hi", false];
-var arr3= new Array();
-var arr4= new Array(4);
+var myCar=new Car();
+myCar.length;
+myCar.maxSpeed;
+myCar.drive();
+myCar.brake(35);
+myCar.topSpeed;
 
-
-console.log(arr);
-console.log(arr2);
-console.log(arr3);
-console.log(arr4);
-console.log(arr2.length);
-console.log(arr2.sort());
-console.log(arr2.reverse());
+var myString= new String();
+myString="Hello";
+myString.length;
+myString.toLowerCase();
+var myString2="hi there";
+myString2.toUpperCase();
