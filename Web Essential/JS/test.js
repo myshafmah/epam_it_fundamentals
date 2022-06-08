74de2ee46a54215c8ad7094a43d0376387874930
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-08 10:49:50 +0300

    JS Creating a new JavaScript Object

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -1,13 +1,21 @@
-var myCar=new Car();
-myCar.length;
-myCar.maxSpeed;
-myCar.drive();
-myCar.brake(35);
-myCar.topSpeed;
+var arr = new Array();
+arr[0] = 8;
+arr[1] = "Hello";
+arr[2] = true;
 
-var myString= new String();
-myString="Hello";
-myString.length;
-myString.toLowerCase();
-var myString2="hi there";
-myString2.toUpperCase();
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+console.log(myCar.driver)
+myCar.drive = function(){console.log("now driving");};
+myCar.drive();
+
+var myCar2 = {
+    maxSpeed: 70, 
+    driver:"Net Ninja", 
+    drive:function(speed, time){
+        console.log(speed*time+ "km");
+    }
+};
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
