9b65aff7f6cadd6b404a18f873e0b8743c116ba0
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-08 10:05:17 +0300

    JS Slice and Split Strings

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -1,15 +1,10 @@
-// var mystring="I\'m a 'fun ninja' string";
-// console.log(mystring.length);
-// console.log(mystring.toUpperCase());
-// console.log(mystring.toLowerCase());
-// console.log(mystring.indexOf("string"));
-// if (mystring.indexOf("ninja")===-1){
-//     console.log("the word ninja is not in the string")
-// }else{
-//     console.log(' the word "ninja" starts at position '+mystring.indexOf('ninja'));
-// }
-var string1="abc";
-var string2="ABC";
-console.log(string1 === string2);
-console.log(string1.toLowerCase() ===string2.toLowerCase());
-console.log(string1<string2);
+var str="hello, world";
+var str2=str.slice(2,9);
+var str3=str.slice(2);
+var tags="meat, ha, salami, pork, beef, chicken";
+var tagsArray=tags.split(",");
+
+console.log(str);
+console.log(str2);
+console.log(str3);
+console.log(tagsArray);
