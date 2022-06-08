393ec0cb2f6176c4a1152d4e0214b76cebe14b44
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-08 11:42:39 +0300

    JS THIS Keyword

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -1,21 +1,31 @@
-var arr = new Array();
-arr[0] = 8;
-arr[1] = "Hello";
-arr[2] = true;
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-console.log(myCar.driver)
-myCar.drive = function(){console.log("now driving");};
-myCar.drive();
+// console.log(this);
 
 var myCar2 = {
     maxSpeed: 70, 
     driver:"Net Ninja", 
     drive:function(speed, time){
         console.log(speed*time+ "km");
+    },
+    // test: function(){
+        // console.log(this);}
+    logDriver: function(){
+        console.log('driver name is ' + this.driver);
     }
 };
+// myCar2.test();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
+
+// var myCar3 = {
+//     maxSpeed: 40, 
+//     driver:"Shaun", 
+//     drive:function(speed, time){
+//         console.log(speed*time+ "km");
+//     },
+//     test: function(){
+//         console.log(this);}
+// };
+
+// myCar2.test();
+// myCar3.test();
+myCar2.logDriver();
