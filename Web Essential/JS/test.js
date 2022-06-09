ebab56992cf1776866a9b367545c3c90821c09b6
Mykhailo Fedoryshyn <fedoryshyn.mykhailo@gmail.com>
2022-06-09 10:32:57 +0300

    JS The Date Object

--- Web Essential/JS/test.js
+++ Web Essential/JS/test.js
@@ -1,20 +1,21 @@
-var Car = function(maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed*time + "km");
-    };
-    this.driver = driver;
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
-}
+var my_date = new Date();
+console.log(my_date);
 
-var myCar = new Car(50, 'ninja man');
-var myCar2 = new Car(50, 'humpty man');
-var myCar3 = new Car(50, 'john man');
-var myCar4 = new Car(50, 'bond man');
-var myCar5 = new Car(50, 'James man');
+var my_past_date=new Date(1545,11, 2, 10, 12, 45);
+var my_future_date= new Date(2515,0,31, 10, 12, 45);
+console.log(my_past_date);
+console.log(my_future_date);
 
-myCar.drive(30,7);
-myCar3.logDriver();
+var birthday = new Date(1985,0,15,11,15,25);
+var birthday2 = new Date(1985,0,15,11,15,25);
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
+
+if (birthday.getTime()==birthday2.getTime()){
+    console.log('birthdays are equal');
+}else{
+    console.log('birthdays are not equal')}
